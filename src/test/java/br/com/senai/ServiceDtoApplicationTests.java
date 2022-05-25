d02e6874e086bd5b0c28ccfd983727b4d465b4d8
roni-info <roni_info@yahoo.com.br>
2022-05-25 18:28:24 -0300

    Criação do Projeto

---
+++ src/test/java/br/com/senai/ServiceDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
